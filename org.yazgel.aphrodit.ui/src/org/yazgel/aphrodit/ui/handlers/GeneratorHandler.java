21ece99656b3ef3a92bd7d203c696b621e1fb0f0
Akgün Demirbaş <akgun.demirbas@gmail.com>
2014-09-13 23:17:07 +0300

    ui handler'da theme generator çağırılıyor.

--- org.yazgel.aphrodit.ui/src/org/yazgel/aphrodit/ui/handlers/GeneratorHandler.java
+++ org.yazgel.aphrodit.ui/src/org/yazgel/aphrodit/ui/handlers/GeneratorHandler.java
@@ -3,13 +3,41 @@ package org.yazgel.aphrodit.ui.handlers;
 import org.eclipse.core.commands.AbstractHandler;
 import org.eclipse.core.commands.ExecutionEvent;
 import org.eclipse.core.commands.ExecutionException;
+import org.eclipse.core.resources.IFile;
+import org.eclipse.core.runtime.IProgressMonitor;
+import org.eclipse.core.runtime.IStatus;
+import org.eclipse.core.runtime.Status;
+import org.eclipse.ui.PlatformUI;
+import org.yazgel.aphrodit.codegenerator.mwe.ThemeGenerator;
+import org.yazgel.util.ui.AbstractJob;
 
 public class GeneratorHandler extends AbstractHandler {
 
 	@Override
 	public Object execute(ExecutionEvent event) throws ExecutionException {
-		/* TODO: Gerceklestir. */
-		System.out.println("Execute handler.");
+
+		new AbstractJob(PlatformUI.getWorkbench().getActiveWorkbenchWindow()) {
+
+			@Override
+			protected IStatus runInsideJob(IProgressMonitor monitor) {
+				final IFile iFile = getIFileSelection().get(0);
+				final String modelPath = iFile.getParent().getLocation()
+						.toString();
+				final String targetDir = iFile.getParent().getLocation()
+						.toString();
+
+				final ThemeGenerator themeGenerator = new ThemeGenerator();
+				themeGenerator.generate(modelPath, targetDir);
+
+				return Status.OK_STATUS;
+			}
+
+			@Override
+			protected String jobName() {
+				return "Generating Code";
+			}
+		}.execute();
+
 		return null;
 	}
 }
